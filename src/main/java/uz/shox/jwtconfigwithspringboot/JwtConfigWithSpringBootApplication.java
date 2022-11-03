e20d927b67d173acd2884dd4a1b18cd512168dcd
shoxrux071 <begratefuller071@gmail.com>
2022-11-03 16:01:13 +0500

    Initial commit

---
+++ src/main/java/uz/shox/jwtconfigwithspringboot/JwtConfigWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uz.shox.jwtconfigwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtConfigWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtConfigWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/shox/jwtconfigwithspringboot/JwtConfigWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.shox.jwtconfigwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtConfigWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
